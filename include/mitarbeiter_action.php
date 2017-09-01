90416956f62a5408e06377651609b9a1b56b258b
Timon <timon.borchers@googlemail.com>
2017-09-01 23:18:32 +0200

    add Check

--- include/mitarbeiter_action.php
+++ include/mitarbeiter_action.php
@@ -24,46 +24,47 @@
 		$junctionNumber = mysqli_real_escape_string ($conn, $junctionNumber);
 		$lat_insert = mysqli_real_escape_string ($conn, $lat_insert);
 		$lon_insert = mysqli_real_escape_string ($conn, $lon_insert);
-		
-		//Start Check TollgateCode
-		$query_getTollgateCode = "SELECT code FROM mautstelle WHERE code = $code";
-		$result_getTollgateCode = mysqli_query($conn, $query_getTollgateCode);
-			$rows = mysqli_num_rows($result_getTollgateCode);
-			if ($rows == 0){
-				$checkTollgateCode = "TRUE";
-				echo "checktollgadeCode TRUE";
-			}
-			if ($rows >= 1){
-				$checkTollgateCode = "FALSE";
-				echo "MautstellenCode ist bereits in der Datenbank";
-			}
-		//End Check TollgateCode
-		if($checkTollgateCode == "TRUE"){
-			if (preg_match("/^(\d{1,2})([.])(\d{1,10})$/", $lat_insert)){
-				$wrongLat = "FALSE";
-			}
-			else
-			{
-				$wrongLat = "TRUE";
-				echo "Latitude hat falsches Format";
-			}
-			
-			if($wrongLat == "FALSE"){
-				if (preg_match("/^(\d{1,2})([.])(\d{1,10})$/", $lon_insert)){
-					$wrongLon = "FALSE";
+		if($code != "" || $namehighway != "" || $namejunction != "" || $lon_insert != "" || $lat_insert != ""){
+			//Start Check TollgateCode
+			$query_getTollgateCode = "SELECT code FROM mautstelle WHERE code = $code";
+			$result_getTollgateCode = mysqli_query($conn, $query_getTollgateCode);
+				$rows = mysqli_num_rows($result_getTollgateCode);
+				if ($rows == 0){
+					$checkTollgateCode = "TRUE";
+					echo "checktollgadeCode TRUE";
+				}
+				if ($rows >= 1){
+					$checkTollgateCode = "FALSE";
+					echo "MautstellenCode ist bereits in der Datenbank";
+				}
+			//End Check TollgateCode
+			if($checkTollgateCode == "TRUE"){
+				if (preg_match("/^(\d{1,2})([.])(\d{1,10})$/", $lat_insert)){
+					$wrongLat = "FALSE";
 				}
 				else
 				{
-					$wrongLon = "TRUE";
-					echo "Longitude hat falsches Format";
+					$wrongLat = "TRUE";
+					echo "Latitude hat falsches Format";
 				}
-			
-				if($wrongLon == "FALSE"){
 				
-					$query_sql_add = "INSERT INTO mautstelle (code, nameAutobahn, nameKreuz, kreuzNummer, lat, lon) VALUES ('$code', '$namehighway', '$namejunction', '$junctionNumber', '$lat_insert', '$lon_insert')";
-					echo $query_sql_add;
-					mysqli_query($conn,$query_sql_add);
-					echo "Mautstelle erfolgreich hinzugefügt";
+				if($wrongLat == "FALSE"){
+					if (preg_match("/^(\d{1,2})([.])(\d{1,10})$/", $lon_insert)){
+						$wrongLon = "FALSE";
+					}
+					else
+					{
+						$wrongLon = "TRUE";
+						echo "Longitude hat falsches Format";
+					}
+				
+					if($wrongLon == "FALSE"){
+					
+						$query_sql_add = "INSERT INTO mautstelle (code, nameAutobahn, nameKreuz, kreuzNummer, lat, lon) VALUES ('$code', '$namehighway', '$namejunction', '$junctionNumber', '$lat_insert', '$lon_insert')";
+						echo $query_sql_add;
+						mysqli_query($conn,$query_sql_add);
+						echo "Mautstelle erfolgreich hinzugefügt";
+					}
 				}
 			}
 		}
