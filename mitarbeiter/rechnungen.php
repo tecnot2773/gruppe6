646321f87b7974db77bcf5b220d4fa9c6658471e
Timon <tecnot2773@users.noreply.github.com>
2017-09-04 08:49:26 +0200

    Update rechnungen.php

--- mitarbeiter/rechnungen.php
+++ mitarbeiter/rechnungen.php
@@ -99,7 +99,7 @@
 										echo "<td width='70px'>" . $data5['nameAutobahn'] . "<br>". $data5['nameKreuz'] . "</td>";
 										echo "<td width='70px'>" . $data6['nameAutobahn'] . "<br>". $data6['nameKreuz'] . "</td>";
 										echo "<td width='70px'>" . $data4['zeitstempel'] . "</td>";
-										echo "<td width='70px'>" . $data1['kosten'] . € . "</td>";
+										echo "<td width='70px'>" . $data1['kosten'] ."<br>". € . "</td>";
 										echo "</tr>";
 									}
 								}
@@ -112,6 +112,5 @@
 			</div>
 		</div>
 		</div>
-		<!-- JAVASCRIPT  -->
 	</body>
 </html>
