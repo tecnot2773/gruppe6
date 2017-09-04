6b47a360cf8f131b8ae1796d7a6c02645ad37d70
Timon <timon.borchers@googlemail.com>
2017-09-04 12:29:09 +0200

    *

--- mitarbeiter/rechnungen.php
+++ mitarbeiter/rechnungen.php
@@ -99,7 +99,7 @@
 										echo "<td width='70px'>" . $data5['nameAutobahn'] . "<br>". $data5['nameKreuz'] . "</td>";
 										echo "<td width='70px'>" . $data6['nameAutobahn'] . "<br>". $data6['nameKreuz'] . "</td>";
 										echo "<td width='70px'>" . $data4['zeitstempel'] . "</td>";
-										echo "<td width='70px'>" . $data1['kosten'] . '&nbsp;' € . "</td>";
+										echo "<td width='70px'>" . $data1['kosten'] . &nbsp; . € . "</td>";
 										echo "</tr>";
 									}
 								}
