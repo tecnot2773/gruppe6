612e9841aad956d4ec7c4bbceef344e24a1b5ecb
Timon <timon.borchers@googlemail.com>
2017-09-13 12:45:34 +0200

    comment autobahn.php

--- mitarbeiter/autobahn.php
+++ mitarbeiter/autobahn.php
@@ -1,10 +1,9 @@
 ﻿<?php
- include_once '../include/db.php';
+ include_once '../include/db.php';																		//include db.php
  
- $query_get_route = "SELECT id, kennzeichen, faehrtEinID FROM strecke WHERE faehrtAusID IS NULL";
- $result_get_route = mysqli_query($conn,$query_get_route);
+ $query_getRoute = "SELECT id, kennzeichen, faehrtEinID FROM strecke WHERE faehrtAusID IS NULL";		//query getRoute
+ $result_getRoute = mysqli_query($conn,$query_getRoute);												//execute Query and save
  
- //$result = utf8ize($result);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -50,7 +49,8 @@
 					<p></p>
 					<div id="buttondiv_rowend" class="row">
 					<center><center>
-					<?php if ($_SERVER['REQUEST_METHOD'] === 'POST'){ include_once '../include/mitarbeiter_action.php'; }?>
+					<?php if ($_SERVER['REQUEST_METHOD'] === 'POST'){ include_once '../include/mitarbeiter_action.php'; } //if REQUEST_METHOD == POST include mitarbeiter_action.php
+					?>	
 					</div>
 				</div>
 			<div id="griddiv-left" class="test">
@@ -63,22 +63,22 @@
 				<th>Einfahrts Zeit</th>
 				</tr>";
 
-				while($data1 = mysqli_fetch_array($result_get_route)){
-					$db_entryID = $data1['faehrtEinID'];
-					$query_get_tollgateEntry = "SELECT zeitstempel, mautstelleID FROM faehrtEin WHERE id = $db_entryID";
-					$result_get_tollgateEntry = mysqli_query($conn, $query_get_tollgateEntry);
-					while($data2 = mysqli_fetch_array($result_get_tollgateEntry)){
-						$db_tollgateID = $data2['mautstelleID'];
-						$query_get_tollgateName = "SELECT nameAutobahn, nameKreuz FROM mautstelle WHERE ID = $db_tollgateID";
-						$result_get_tollgateName = mysqli_query($conn, $query_get_tollgateName);
-						while($data3 = mysqli_fetch_array($result_get_tollgateName)){
+				while($data1 = mysqli_fetch_array($result_getRoute)){																//fetch result_getRoute
+					$db_entryID = $data1['faehrtEinID'];																			//save faehrtEinID in db_entryID
+					$query_getTollgateEntry = "SELECT zeitstempel, mautstelleID FROM faehrtEin WHERE id = $db_entryID";				//query getTollgateEntry
+					$result_getTollgateEntry = mysqli_query($conn, $query_getTollgateEntry);										//execute query and save
+					while($data2 = mysqli_fetch_array($result_getTollgateEntry)){													//fetch result_getTollgateEntry
+						$db_tollgateID = $data2['mautstelleID'];																	//save mautstelleID in db_tollgateID
+						$query_getTollgateName = "SELECT nameAutobahn, nameKreuz FROM mautstelle WHERE ID = $db_tollgateID";		//query getTollgateName
+						$result_getTollgateName = mysqli_query($conn, $query_getTollgateName);										//execute query and save
+						while($data3 = mysqli_fetch_array($result_getTollgateName)){												//fetch result_getTollgateName
 
 						echo "<tr class='userlistoutput'>";
 						
-						echo "<td width='45px'>" . $data1['id'] . "</td>"; 
-						echo "<td width='45px'>" . $data1['kennzeichen'] . "</td>";
-						echo "<td width='70px'>" . $data3['nameAutobahn'] . "<br>". $data3['nameKreuz'] . "</td>";
-						echo "<td width='70px'>" . $data2['zeitstempel'] . "</td>";
+						echo "<td width='45px'>" . $data1['id'] . "</td>"; 															//echo id
+						echo "<td width='45px'>" . $data1['kennzeichen'] . "</td>";													//echo kennzeichen
+						echo "<td width='70px'>" . $data3['nameAutobahn'] . "<br>". $data3['nameKreuz'] . "</td>";					//echo nameAutobahn
+						echo "<td width='70px'>" . $data2['zeitstempel'] . "</td>";													//echo zeitstempel
 						echo "</tr>";
 						}
 					}
