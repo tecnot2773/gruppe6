2e6b442b7d674bf2a3d83a66c69bca4082773337
Timon <timon.borchers@googlemail.com>
2017-09-15 00:57:40 +0200

    *

--- statistics.php
+++ statistics.php
@@ -4,17 +4,17 @@ include_once 'include/db.php';
 $query_tollgateNumber = "SELECT * FROM mautstelle";
 $result_tollgateNumber = mysqli_query($conn,$query_tollgateNumber);
 $tollgateNumberRows = mysqli_num_rows($result_tollgateNumber);
-echo "Es sind ${tollgateNumberRows} Mautstellen in der Datenbank" . "<br>" . PHP_EOL;
+echo "Es sind ${tollgateNumberRows} Mautstellen in der Datenbank." . "<br>" . PHP_EOL;
 
 $query_entryNumber = "SELECT * FROM faehrtEin";
 $result_entryNumber = mysqli_query($conn,$query_entryNumber);
 $entryNumberRows = mysqli_num_rows($result_entryNumber);
-echo "Es sind insgesamt ${entryNumberRows} Einfahrten verbucht";
+echo "Es sind insgesamt ${entryNumberRows} Einfahrten verbucht" . "<br>" . PHP_EOL;
 
 $query_exitNumber = "SELECT * FROM faehrtAus";
 $result_exitNumber = mysqli_query($conn,$query_exitNumber);
 $exitNumberRows = mysqli_num_rows($result_exitNumber);
-echo "Es sind insgesamt ${exitNumberRows} Ausfahrten verbucht";
+echo "Es sind insgesamt ${exitNumberRows} Ausfahrten verbucht" . "<br>" . PHP_EOL;
 $onTheRoad = $entryNumberRows - $exitNumberRows;
-echo "Derzeit sind also ${onTheRoad} Fahrzeuge auf der Autobahn";
+echo "Derzeit sind also ${onTheRoad} Fahrzeuge auf der Autobahn" . "<br>" . PHP_EOL;
 ?>
