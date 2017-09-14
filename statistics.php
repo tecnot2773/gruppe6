dc1d0c4a2c4ae4d5104a572c2388991e54d788f9
Timon <timon.borchers@googlemail.com>
2017-09-14 23:45:38 +0200

    start statistics

---
+++ statistics.php
@@ -0,0 +1,9 @@
+﻿<?php
+include_once 'db.php';
+
+$query_tollgateNumber = "SELECT * FROM mautstelle";
+$result_tollgateNumber = mysqli_query($conn,$query_tollgateNumber);
+$tollgateNumberRows = mysqli_num_rows($result_tollgateNumber);
+echo "Es sind $tollgateNumberRows Mautstellen in der Datenbank";
+
+?>
