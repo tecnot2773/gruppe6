0c754f60a79d7c6a152ab63404c8a65a4c4f62fc
Timon <timon.borchers@googlemail.com>
2017-09-15 00:55:39 +0200

    *

--- statistics.php
+++ statistics.php
@@ -1,13 +1,10 @@
-﻿<!DOCTYPE html>
-<html lang="en">
-
-<?php
+﻿<?php
 include_once 'include/db.php';
 
 $query_tollgateNumber = "SELECT * FROM mautstelle";
 $result_tollgateNumber = mysqli_query($conn,$query_tollgateNumber);
 $tollgateNumberRows = mysqli_num_rows($result_tollgateNumber);
-echo "Es sind ${tollgateNumberRows} Mautstellen in der Datenbank" .<br>;
+echo "Es sind ${tollgateNumberRows} Mautstellen in der Datenbank" .<br>. PHP_EOL;
 
 $query_entryNumber = "SELECT * FROM faehrtEin";
 $result_entryNumber = mysqli_query($conn,$query_entryNumber);
@@ -21,5 +18,3 @@ echo "Es sind insgesamt ${exitNumberRows} Ausfahrten verbucht";
 $onTheRoad = $entryNumberRows - $exitNumberRows;
 echo "Derzeit sind also ${onTheRoad} Fahrzeuge auf der Autobahn";
 ?>
-
-</html>
