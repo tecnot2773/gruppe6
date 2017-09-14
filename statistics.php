5c839918f8b309013149a637c1a9a875c2b79f00
Timon <timon.borchers@googlemail.com>
2017-09-14 23:52:50 +0200

    *

--- statistics.php
+++ statistics.php
@@ -1,5 +1,5 @@
 ﻿<?php
-include_once 'db.php';
+include_once '/include/db.php';
 
 $query_tollgateNumber = "SELECT * FROM mautstelle";
 $result_tollgateNumber = mysqli_query($conn,$query_tollgateNumber);
