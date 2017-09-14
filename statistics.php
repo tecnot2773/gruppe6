de2c3ec8891567ebe610513c7c3df8566532b8b3
Timon <timon.borchers@googlemail.com>
2017-09-15 00:41:57 +0200

    *

--- statistics.php
+++ statistics.php
@@ -7,7 +7,8 @@ include_once 'include/db.php';
 $query_tollgateNumber = "SELECT * FROM mautstelle";
 $result_tollgateNumber = mysqli_query($conn,$query_tollgateNumber);
 $tollgateNumberRows = mysqli_num_rows($result_tollgateNumber);
-echo "Es sind ${tollgateNumberRows} Mautstellen in der Datenbank"; <br>
+echo "Es sind ${tollgateNumberRows} Mautstellen in der Datenbank";
+echo  <br>;
 
 $query_entryNumber = "SELECT * FROM faehrtEin";
 $result_entryNumber = mysqli_query($conn,$query_entryNumber);
