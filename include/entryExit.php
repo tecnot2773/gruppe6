ed158432c31ed03c0ca52d2cfd9eb7354efea51d
Timon <timon.borchers@googlemail.com>
2017-09-15 22:32:05 +0200

    modify to adapt at new changes

--- include/entryExit.php
+++ include/entryExit.php
@@ -5,63 +5,6 @@
 	//Add new tollgate
 	$action= $_POST["selection"];
 
-	
-	if ($action == "add")
-	{
-		$code = $_POST["text-code"];
-		$namehighway = $_POST["text-namehighway"];
-		$namejunction = $_POST["text-namejunction"];
-		$junctionNumber = $_POST["text-junctionNumber"];
-		$lat_insert = $_POST["text-lat"];
-		$lon_insert = $_POST["text-lon"];
-		
-		$code = mysqli_real_escape_string ($conn, $code);
-		$namehighway = mysqli_real_escape_string ($conn, $namehighway);
-		$namejunction = mysqli_real_escape_string ($conn, $namejunction);
-		$junctionNumber = mysqli_real_escape_string ($conn, $junctionNumber);
-		$lat_insert = mysqli_real_escape_string ($conn, $lat_insert);
-		$lon_insert = mysqli_real_escape_string ($conn, $lon_insert);
-		//Start Check TollgateCode
-		$query_getTollgateCode = "SELECT code FROM mautstelle WHERE code = $code";
-		$result_getTollgateCode = mysqli_query($conn, $query_getTollgateCode);
-		$rows = mysqli_num_rows($result_getTollgateCode);
-		if ($rows == 0){
-			$checkTollgateCode = "TRUE";
-		}
-		if ($rows >= 1){
-			$checkTollgateCode = "FALSE";
-			echo "MautstellenCode ist bereits in der Datenbank";
-		}
-		//End Check TollgateCode
-		if($checkTollgateCode == "TRUE"){
-			if (preg_match("/^(\d{1,2})([.])(\d{1,10})$/", $lat_insert)){
-				$wrongLat = "FALSE";
-			}
-			else
-			{
-				$wrongLat = "TRUE";
-				echo "Latitude hat falsches Format";
-			}
-			
-			if($wrongLat == "FALSE"){
-				if (preg_match("/^(\d{1,2})([.])(\d{1,10})$/", $lon_insert)){
-					$wrongLon = "FALSE";
-				}
-				else
-				{
-					$wrongLon = "TRUE";
-					echo "Longitude hat falsches Format";
-				}
-			
-				if($wrongLon == "FALSE"){
-				
-					$query_sql_add = "INSERT INTO mautstelle (code, nameAutobahn, nameKreuz, kreuzNummer, lat, lon) VALUES ('$code', '$namehighway', '$namejunction', '$junctionNumber', '$lat_insert', '$lon_insert')";
-					mysqli_query($conn,$query_sql_add);
-					echo "Mautstelle erfolgreich hinzugefügt";
-				}
-			}
-		}
-	}
 	//add new vehicle entry
 	if ($action == "entry")
 	{
