3a51481ee6bc6ad167847425c352902eecb903d8
Timon <timon.borchers@googlemail.com>
2017-09-15 17:50:58 +0200

    remove old output

--- include/statistics.php
+++ include/statistics.php
@@ -4,19 +4,15 @@ include_once 'db.php';
 $query_tollgateNumber = "SELECT * FROM mautstelle";
 $result_tollgateNumber = mysqli_query($conn,$query_tollgateNumber);
 $tollgateNumberRows = mysqli_num_rows($result_tollgateNumber);
-echo "Es sind ${tollgateNumberRows} Mautstellen in der Datenbank." . "<br>" . PHP_EOL;
 
 $query_entryNumber = "SELECT * FROM faehrtEin";
 $result_entryNumber = mysqli_query($conn,$query_entryNumber);
 $entryNumberRows = mysqli_num_rows($result_entryNumber);
-echo "Es sind insgesamt ${entryNumberRows} Einfahrten verbucht." . "<br>" . PHP_EOL;
 
 $query_exitNumber = "SELECT * FROM faehrtAus";
 $result_exitNumber = mysqli_query($conn,$query_exitNumber);
 $exitNumberRows = mysqli_num_rows($result_exitNumber);
-echo "Es sind insgesamt ${exitNumberRows} Ausfahrten verbucht." . "<br>" . PHP_EOL;
 $onTheRoad = $entryNumberRows - $exitNumberRows;
-echo "Derzeit sind also ${onTheRoad} Fahrzeuge auf der Autobahn." . "<br>" . PHP_EOL;
 
 $currentMonth = date("Y-m");
 $currentDay = date("Y-m-d");
@@ -24,12 +20,10 @@ $currentDay = date("Y-m-d");
 $query_getDailyExit = "SELECT * FROM faehrtAus WHERE zeitstempel LIKE '$currentDay%'";
 $result_getDailyExit = mysqli_query($conn,$query_getDailyExit);
 $dailyExitRows = mysqli_num_rows($result_getDailyExit);
-echo "Heute sind ${dailyExitRows} Autos über die Autobahn gefahren." . "<br>" . PHP_EOL;
 
 $query_getMonthlyExit = "SELECT * FROM faehrtAus WHERE zeitstempel LIKE '$currentMonth%'";
 $result_getMonthlyExit = mysqli_query($conn,$query_getMonthlyExit);
 $monthlyExitRows = mysqli_num_rows($result_getMonthlyExit);
-echo "Diesen Monat sind ${monthlyExitRows} Autos über die Autobahn gefahren." . "<br>" . PHP_EOL;
 
 echo "<table border='1'>
 <tr>
