3e40c4675230e1c4f64086cb7896b76ae9697f6b
Timon <timon.borchers@googlemail.com>
2017-09-16 19:36:53 +0200

    remove old preg_match

--- include/include_entryExit.php
+++ include/include_entryExit.php
@@ -56,8 +56,6 @@
 					else{
 						$entry_time = $entry_time;
 					}
-					
-					//if (preg_match("/^(\d{4})([.])(\d{2})([.])(\d{2})(\s)(\d{2})([:])(\d{2})([:])(\d{2})$/", $entry_time)){
 					if (preg_match("/^(\d{2})([.])(\d{2})([.])(\d{4})(\s)(\d{2})([:])(\d{2})([:])(\d{2})$/", $entry_time)){	
 					}
 					else
