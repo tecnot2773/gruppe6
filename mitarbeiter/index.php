d6f5ee38c6d7ede09caa413268cfbd3b9a9aace6
Timon <timon.borchers@googlemail.com>
2017-09-17 11:45:12 +0200

    *

--- mitarbeiter/index.php
+++ mitarbeiter/index.php
@@ -65,7 +65,7 @@
 					<div id="rowend" class="row">
 						<select name="text-plate-exit" class="enjoy-css">
 							<option selected="selected"></option>
-							<option value="" disabled selected>Select something...</option>
+							<option value="" disabled selected hidden>Kennzeichen</option>
 							<?php 
 								include_once '../include/include_db.php';																	//create db connection
 								$query_getPlate = "SELECT kennzeichen from strecke WHERE faehrtAusID IS NULL";						//sql query to get  kennzeichen
