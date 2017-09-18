c54b09fa3a2d9b3955114fc8e28fb690be7bbf0e
FranklinClinton <joern@team-runge.de>
2017-09-18 21:43:45 +0200

    fixxed this shit lol

--- mitarbeiter/addmaut.php
+++ mitarbeiter/addmaut.php
@@ -5,7 +5,6 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<link rel="shortcut icon" type="image/x-icon" href="/../images/favicon.ico">
-		<link href="/mitarbeiter/addmaut.css" type="text/css" rel="stylesheet" />
 		<link href="/mitarbeiter/textbox.css" type="text/css" rel="stylesheet" />
 		<link href="/css/griddiv-nav.css" type="text/css" rel="stylesheet" />
 		<link href="/css/buttons.css" type="text/css" rel="stylesheet" />
