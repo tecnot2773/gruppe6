d4314352f8d5d82a0c783d7a1b6781485ddfdc35
Timon <timon.borchers@googlemail.com>
2017-10-01 18:26:50 +0200

    *

--- stations/check_ffn.php
+++ stations/check_ffn.php
@@ -1,10 +1,49 @@
 <?php
+	
+	$servername = "localhost";														//server name
+	$username = "stationssql";														//login name
+	$password = "veve113ppe";														//login password
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);							//error log
+	}
+	$conn->select_db("stations");													//selet Database
+	
+	$strQuery = "SET character_set_results = 'utf8', 
+	  character_set_client = 'utf8', 
+	  character_set_connection = 'utf8', 
+	  character_set_database = 'utf8', 
+	  character_set_server = 'utf8'";
+	$conn->query($strQuery);  
+	
+	
+	$station = 1;
+	
 	$http_content = file_get_contents("https://www.ffn.de/musik/playlist/");
 	preg_match('/<p class="title">(.*)/', $http_content, $songs);
 	preg_match('/<p class="title">(.*)/', $http_content, $artists);
-	echo($songs[0]);
+	$songName = ($songs[0]);
 	echo "<br>";
 	echo($artists[0]);
-
- 
+	$query_getSongId = "SELECT id FROM song WHERE name = $songName";
+	$result_getSongId = mysqli_query($conn, $query_getSongId);
+	$rows_getSongId = mysqli_num_rows($result_getSongId);
+	if($rows_getSongId >= 0){
+		while ($data = mysqli_fetch_array($result_getSongId)){
+			$db_songId = $data['id'];
+		}
+		$query_getLastSong = "SELECT FROM plays WHERE stationId = $station and songId = $db_songId";
+		$result_getLastSong = mysqli_query($conn, $query_getLastSong);
+		while ($data = mysqli_fetch_array($result_getLastSong)){
+			$db_songId2 = $data['id'];
+		}
+		if($db_songId == $db_songId2){
+			$time = time();
+			fopen("https://gruppe6.torutec.eu/stations/plays.php?songname=" . $songName . "&station=" . $station . "&time=" . $time, "r");
+		}
+	}
+	
 ?>
--- stations/plays.php
+++ stations/plays.php
@@ -1,95 +1,95 @@
 <?php
-include_once 'db.php';
+	include_once 'db.php';
 
-$songname = $_GET["songname"];
-$time = $_GET["time"];
-$station = $_GET["station"];
+	$songname = $_GET["songname"];
+	$time = $_GET["time"];
+	$station = $_GET["station"];
 
-$currentMonth = date("Y-m");
-$currentDay = date("Y-m-d");
-$currentHour = date("Y-m-d H");
-$currentSeconds = date("Y-m-d H:i:s");
+	$currentMonth = date("Y-m");
+	$currentDay = date("Y-m-d");
+	$currentHour = date("Y-m-d H");
+	$currentSeconds = date("Y-m-d H:i:s");
 
-$query_getSongId = "SELECT id FROM song WHERE name = '$songname'";
-$result_getSongId = mysqli_query($conn, $query_getSongId);
-$rows_getSongid = mysqli_num_rows($result_getSongId);
-if ($rows_getSongid == 0){
-	$query_insertSong = "INSERT INTO song (name) VALUES ('$songname')";
-	mysqli_query($conn, $query_insertSong);
-	$get_songId = mysqli_query($conn, $query_getSongId);
-	while($data = mysqli_fetch_array($get_songId)){
-		$db_songId = $data['id'];
-	}
-}
-else{
-	while($data = mysqli_fetch_array($result_getSongId)){
-		$db_songId = $data['id'];
-	}
-}
-$timestamp = date("Y-m-d H:i:s", $time);
-if (empty($timestamp)){
-	$timestamp = date("Y-m-d H:i:s");
-}
-
-$query_insertPlays = "INSERT INTO plays (stationId, songId, timestamp) VALUES ('$station', '$db_songId', '$timestamp')";
-mysqli_query($conn, $query_insertPlays);
-
-$query_checkPlaysHour = "SELECT songId FROM plays WHERE stationId = '$station' AND timestamp LIKE '%$currentHour%'";
-$result_checkPlaysHour = mysqli_query($conn, $query_checkPlaysHour);
-$playsHourRows = mysqli_num_rows($result_checkPlaysHour);
-
-$query_checkPlaysDay = "SELECT songId FROM plays WHERE stationId = '$station' AND timestamp LIKE '%$currentDay%'";
-$result_checkPlaysDay = mysqli_query($conn, $query_checkPlaysDay);
-$playsDayRows = mysqli_num_rows($result_checkPlaysDay);
-
-$dailystatsExists = mysqli_query($conn, "SELECT * FROM dailyStats WHERE stationId = '$station' and timestamp LIKE '$currentDay%'");
-$dailystatsRows = mysqli_num_rows($dailystatsExists);
-if($dailystatsRows == 0){
-	mysqli_query($conn, "INSERT INTO dailyStats (stationId, timestamp, replaysPerHour, replaysPerDay, mostReplaysDuring, score) VALUES ('$station', '$currentSeconds', '0', '0', '0', '0')");
-}
-
-if($playsHourRows >= 1){
-	mysqli_query($conn, "UPDATE dailyStats SET replaysPerDay = replaysPerDay + 1, replaysPerHour = replaysPerHour +1 WHERE stationId = '$station'");
-}
-elseif($playsDayRows >= 1){
-	mysqli_query($conn, "UPDATE dailyStats SET replaysPerDay = replaysPerDay + 1 WHERE stationId = '$station'");
-}
-
-$query_getPlaysDAY = "	SELECT `songId`,
-						COUNT(`songId`) AS `value_occurrence` 
-						FROM `plays`
-						WHERE `stationId`= '$station' AND timestamp LIKE '$currentDay%'
-						GROUP BY `songId`
-						ORDER BY `value_occurrence` DESC
-						LIMIT    1";
-$getmostPlaysDAY = mysqli_query($conn, $query_getPlaysDAY);
-while($data = mysqli_fetch_array($getmostPlaysDAY)){
-	$mostPlaysDAY = $data['songId'];
-}
-mysqli_query($conn,"UPDATE dailyStats SET mostPlayedSong = '$mostPlaysDAY' WHERE stationId = '$station'");
-
-$runs = "24";
-$save_mostPlaysDuring = 0;
-WHILE($runs > 0){
-	$runs = $runs - 1;
-	if($runs >= 10){
-	$day = $currentDay . " " . $runs;
+	$query_getSongId = "SELECT id FROM song WHERE name = '$songname'";
+	$result_getSongId = mysqli_query($conn, $query_getSongId);
+	$rows_getSongid = mysqli_num_rows($result_getSongId);
+	if ($rows_getSongid == 0){
+		$query_insertSong = "INSERT INTO song (name) VALUES ('$songname')";
+		mysqli_query($conn, $query_insertSong);
+		$get_songId = mysqli_query($conn, $query_getSongId);
+		while($data = mysqli_fetch_array($get_songId)){
+			$db_songId = $data['id'];
+		}
 	}
 	else{
-		$day = $currentDay . " 0" . $runs;
+		while($data = mysqli_fetch_array($result_getSongId)){
+			$db_songId = $data['id'];
+		}
 	}
-	$query_mostPlaysDuring = "	SELECT COUNT(`songId`)
-								FROM `plays`
-								WHERE `stationId` = '$station' AND timestamp LIKE '$day%'";
-	$getMostPlaysDuring = mysqli_query($conn, $query_mostPlaysDuring);
-	while($data = mysqli_fetch_array($getMostPlaysDuring)){
-		$mostPlaysDuring = $data['COUNT(`songId`)'];
-	}
-	if($mostPlaysDuring > $save_mostPlaysDuring){
-		$save_mostPlaysDuring = $mostPlaysDuring;
-		$saveTime = $runs;
+	$timestamp = date("Y-m-d H:i:s", $time);
+	if (empty($timestamp)){
+		$timestamp = date("Y-m-d H:i:s");
 	}
 
-}
-mysqli_query($conn, "UPDATE dailyStats SET mostReplaysDuring = '$saveTime' WHERE stationId = '$station'");
+	$query_insertPlays = "INSERT INTO plays (stationId, songId, timestamp) VALUES ('$station', '$db_songId', '$timestamp')";
+	mysqli_query($conn, $query_insertPlays);
+
+	$query_checkPlaysHour = "SELECT songId FROM plays WHERE stationId = '$station' AND timestamp LIKE '%$currentHour%'";
+	$result_checkPlaysHour = mysqli_query($conn, $query_checkPlaysHour);
+	$playsHourRows = mysqli_num_rows($result_checkPlaysHour);
+
+	$query_checkPlaysDay = "SELECT songId FROM plays WHERE stationId = '$station' AND timestamp LIKE '%$currentDay%'";
+	$result_checkPlaysDay = mysqli_query($conn, $query_checkPlaysDay);
+	$playsDayRows = mysqli_num_rows($result_checkPlaysDay);
+
+	$dailystatsExists = mysqli_query($conn, "SELECT * FROM dailyStats WHERE stationId = '$station' and timestamp LIKE '$currentDay%'");
+	$dailystatsRows = mysqli_num_rows($dailystatsExists);
+	if($dailystatsRows == 0){
+		mysqli_query($conn, "INSERT INTO dailyStats (stationId, timestamp, replaysPerHour, replaysPerDay, mostReplaysDuring, score) VALUES ('$station', '$currentSeconds', '0', '0', '0', '0')");
+	}
+
+	if($playsHourRows >= 1){
+		mysqli_query($conn, "UPDATE dailyStats SET replaysPerDay = replaysPerDay + 1, replaysPerHour = replaysPerHour +1 WHERE stationId = '$station'");
+	}
+	elseif($playsDayRows >= 1){
+		mysqli_query($conn, "UPDATE dailyStats SET replaysPerDay = replaysPerDay + 1 WHERE stationId = '$station'");
+	}
+
+	$query_getPlaysDAY = "	SELECT `songId`,
+							COUNT(`songId`) AS `value_occurrence` 
+							FROM `plays`
+							WHERE `stationId`= '$station' AND timestamp LIKE '$currentDay%'
+							GROUP BY `songId`
+							ORDER BY `value_occurrence` DESC
+							LIMIT    1";
+	$getmostPlaysDAY = mysqli_query($conn, $query_getPlaysDAY);
+	while($data = mysqli_fetch_array($getmostPlaysDAY)){
+		$mostPlaysDAY = $data['songId'];
+	}
+	mysqli_query($conn,"UPDATE dailyStats SET mostPlayedSong = '$mostPlaysDAY' WHERE stationId = '$station'");
+
+	$runs = "24";
+	$save_mostPlaysDuring = 0;
+	WHILE($runs > 0){
+		$runs = $runs - 1;
+		if($runs >= 10){
+		$day = $currentDay . " " . $runs;
+		}
+		else{
+			$day = $currentDay . " 0" . $runs;
+		}
+		$query_mostPlaysDuring = "	SELECT COUNT(`songId`)
+									FROM `plays`
+									WHERE `stationId` = '$station' AND timestamp LIKE '$day%'";
+		$getMostPlaysDuring = mysqli_query($conn, $query_mostPlaysDuring);
+		while($data = mysqli_fetch_array($getMostPlaysDuring)){
+			$mostPlaysDuring = $data['COUNT(`songId`)'];
+		}
+		if($mostPlaysDuring > $save_mostPlaysDuring){
+			$save_mostPlaysDuring = $mostPlaysDuring;
+			$saveTime = $runs;
+		}
+
+	}
+	mysqli_query($conn, "UPDATE dailyStats SET mostReplaysDuring = '$saveTime' WHERE stationId = '$station'");
 ?>
