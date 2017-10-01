f8da4acc359c9e21591c9c792594a84360e66410
FranklinClinton <joern@team-runge.de>
2017-10-02 00:26:41 +0200

    *

--- stations/check_bremennext.php
+++ stations/check_bremennext.php
@@ -7,7 +7,7 @@
 	$http_content = file_get_contents("http://www.radiobremen.de/bremennext/titelsuche/titelsuche120.html");
 	preg_match('/\<span class=\"tracktime\"\>.*\<\/span\>(.*)\s*\<\/strong\>/', $http_content, $songs);
 	preg_match('/top44_table_zelle  bottom">(.+?)(?=<)/', $http_content, $artists);
-	echo($songs);
+	echo($songs[2]);
 	
 	
 	die();
