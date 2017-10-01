112941530f779baacf99476c238aef871d0b3f16
FranklinClinton <joern@team-runge.de>
2017-10-01 17:46:47 +0200

    wrong regex

--- stations/check_ffn.php
+++ stations/check_ffn.php
@@ -1,7 +1,7 @@
 <?php
 	$http_content = file_get_contents("https://www.ffn.de/musik/playlist/");
 	preg_match('/<p class="title">(.*)/', $http_content, $songs);
-	preg_match('/<p class="title">(.*)/', $http_content, $artists);
+	preg_match('/<h6 class="artist">(.*)/', $http_content, $artists);
 	echo($songs[0]);
 	echo "<br>";
 	echo($artists[0]);
