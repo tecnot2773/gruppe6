af2812bcfa1fa24f98ff31646a2c51d8e93c37be
FranklinClinton <joern@team-runge.de>
2017-10-01 18:21:28 +0200

    *

--- stations/check_ffn.php
+++ stations/check_ffn.php
@@ -2,9 +2,9 @@
 	$http_content = file_get_contents("https://www.ffn.de/musik/playlist/");
 	preg_match('/<p class="title">(.*)/', $http_content, $songs);
 	preg_match('/<h6 class="artist">(.*)/', $http_content, $artists);
-	echo($songs[0]);
+	echo(strip_tags($songs[0]));
 	echo "<br>";
-	echo($artists[0]);
+	echo(strip_tags($artists[0]));
 
  
 ?>
