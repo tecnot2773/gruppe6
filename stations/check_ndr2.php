0df74b7df15aace2c8e6b8eee8184bba343e608b
Timon <timon.borchers@googlemail.com>
2017-10-01 20:24:18 +0200

    check ndr2

---
+++ stations/check_ndr2.php
@@ -0,0 +1,52 @@
+<?php
+	
+	$servername = "localhost";														//server name
+	$username = "stationssql";														//login name
+	$password = "veve113ppe";														//login password
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);							//error log
+	}
+	$conn->select_db("stations");													//selet Database
+	
+	$strQuery = "SET character_set_results = 'utf8', 
+	  character_set_client = 'utf8', 
+	  character_set_connection = 'utf8', 
+	  character_set_database = 'utf8', 
+	  character_set_server = 'utf8'";
+	$conn->query($strQuery);  
+	
+	$station = 3;
+	$db_currentSongId = 0;
+	$db_lastSongId = 0;
+	
+	$http_content = file_get_contents("https://www.ffn.de/musik/playlist/");
+	preg_match('/<span class="title">(.*)/', $http_content, $songs);
+	preg_match('/<span class="artist">(.*)/', $http_content, $artists);
+	
+	$artistName = strtolower(strip_tags($artists[0]));
+	$artistname = mysqli_real_escape_string($conn, $artistName);
+	
+	$songName = strtolower(strip_tags($songs[0]));
+	$songname = mysqli_real_escape_string($conn, $songName);
+
+	$query_getSongId = "SELECT id FROM song WHERE name = '$songName'";
+	$result_getSongId = mysqli_query($conn, $query_getSongId);
+	if(mysqli_num_rows($result_getSongId) >= 1){ 
+		while ($data = mysqli_fetch_array($result_getSongId)){
+			$db_currentSongId = $data['id'];
+		}
+		$query_getLastSong = "SELECT songId FROM plays WHERE `stationId` = '$station' ORDER BY `timestamp` LIMIT 1";
+		$result_getLastSong = mysqli_query($conn, $query_getLastSong);
+		while ($data = mysqli_fetch_array($result_getLastSong)){
+			$db_lastSongId = $data['songId'];
+		}
+	}
+	if($db_currentSongId == $db_lastSongId){
+		$time = time();
+		include_once "plays.php";
+	}
+?>
