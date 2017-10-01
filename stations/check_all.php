a68c80777259d132f3435ca954b7cc78c56837f3
Timon <timon.borchers@googlemail.com>
2017-10-01 21:18:34 +0200

    check all

---
+++ stations/check_all.php
@@ -0,0 +1,25 @@
+ <?php
+
+	$servername = "localhost";														//server name
+	$username = "stationssql";														//login name
+	$password = "veve113ppe";														//login password
+
+	// Create connection
+	$conn = new mysqli($servername, $username, $password);
+	// Check connection
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);							//error log
+	}
+	$conn->select_db("stations");													//selet Database
+	
+	$strQuery = "SET character_set_results = 'utf8', 
+	  character_set_client = 'utf8', 
+	  character_set_connection = 'utf8', 
+	  character_set_database = 'utf8', 
+	  character_set_server = 'utf8'";
+	$conn->query($strQuery);  
+
+	include_once "check_ffn.php"
+	include_once "check_ndr2.php"
+	include_once "check_njoy.php"
+?> 
