766956d22b0af0924cf292ae65222fc48f309172
Timon <timon.borchers@googlemail.com>
2017-10-02 15:59:38 +0200

    *

--- stations/check_1live.php
+++ stations/check_1live.php
@@ -11,12 +11,12 @@
 	$artistname = mysqli_real_escape_string($conn,strtolower(strip_tags($artists[1])));
 	$songname = mysqli_real_escape_string($conn,strtolower(strip_tags($songs[1])));
 
-	if(preg_match('/\w+(.+?)(?=&)(.+?)(?<=;)/', $artistname){
+	if(preg_match('/\w+(.+?)(?=&)(.+?)(?<=;)/', $artistname)){
 		$pattern = '/\w+(.+?)(?=&)(.+?)(?<=;)/';
 		$replacement = '${2}&';
 		preg_replace($pattern, $replacement, $artistname);
 	}
-	if(preg_match('/\w+(.+?)(?=&)(.+?)(?<=;)/', $songname){
+	if(preg_match('/\w+(.+?)(?=&)(.+?)(?<=;)/', $songname)){
 		$pattern = '/\w+(.+?)(?=&)(.+?)(?<=;)/';
 		$replacement = '${2}&';
 		preg_replace($pattern, $replacement, $songname);
