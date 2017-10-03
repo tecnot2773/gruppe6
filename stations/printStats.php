4dc081fd225d36b8966d11f5e163f9c081b5dde3
Timon <timon.borchers@googlemail.com>
2017-10-04 01:16:12 +0200

    *

--- stations/printStats.php
+++ stations/printStats.php
@@ -3,9 +3,13 @@ include_once "db.php";
 for($i = 1; $i <= 9; $i++){
 	$station = $i;
 	$time = date("Y-m-d H:i:s",strtotime("-10 minutes",strtotime(date("Y-m-d H:i:s"))));
-	echo $time;
-	$result_getLastPlayTime = mysqli_query($conn, "SELECT * FROM plays WHERE stationId = '$station' AND timestamp LIKE '$time' LIMIT 1");
-	
+	$result_getLastPlayTime = mysqli_query($conn, "SELECT * FROM plays WHERE stationId = '1' AND timestamp > '$time%' ORDER BY timestamp DESC LIMIT 1");
+	while($data = mysqli_fetch_array($result_getLastPlayTime)){
+				$db_lastTimestamp = $data['timestamp'];)
+	if(mysqli_num_rows($result_getLastPlayTime)==1){
+		$stationName = mysqli_query($conn, "SELECT name FROM station WHERE id = '$station'");
+		echo $stationName . "hat um " . $db_timestamp  . "das letzte mal die Playlist aktualisiert." . "<br>";
+	}
 }
 
 
