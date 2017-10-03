b29e6b0b3c899b9000e7077ee9f5951bc584a079
Timon <timon.borchers@googlemail.com>
2017-10-03 12:46:54 +0200

    empty check maybe revert

--- stations/check_1live.php
+++ stations/check_1live.php
@@ -31,26 +31,30 @@
 		$replacement = "'";
 		$songname = preg_replace($pattern, $replacement, $songname);
 	}
-	$query_getSongId = "SELECT id FROM song WHERE name = '$songname' AND artist = '$artistname'";
-	$result_getSongId = mysqli_query($conn, $query_getSongId);
-	if(mysqli_num_rows($result_getSongId) >= 1){ 
-		while ($data = mysqli_fetch_array($result_getSongId)){
-			$db_currentSongId = $data['id'];
+	if(empty($artistname) OR empty($songname)){
+		die();
+	}else{
+		$query_getSongId = "SELECT id FROM song WHERE name = '$songname' AND artist = '$artistname'";
+		$result_getSongId = mysqli_query($conn, $query_getSongId);
+		if(mysqli_num_rows($result_getSongId) >= 1){ 
+			while ($data = mysqli_fetch_array($result_getSongId)){
+				$db_currentSongId = $data['id'];
+			}
+			$query_getLastSong = "SELECT songId FROM plays WHERE `stationId` = '$station' ORDER BY `timestamp` DESC LIMIT 1";
+			$result_getLastSong = mysqli_query($conn, $query_getLastSong);
+			while ($data = mysqli_fetch_array($result_getLastSong)){
+				$db_lastSongId = $data['songId'];
+			}
 		}
-		$query_getLastSong = "SELECT songId FROM plays WHERE `stationId` = '$station' ORDER BY `timestamp` DESC LIMIT 1";
-		$result_getLastSong = mysqli_query($conn, $query_getLastSong);
-		while ($data = mysqli_fetch_array($result_getLastSong)){
-			$db_lastSongId = $data['songId'];
+		if($db_currentSongId != $db_lastSongId OR mysqli_num_rows($result_getSongId) == 0){
+			$time = time();
+			include "plays.php";
+			echo "done 1live";
+			echo "<br>";
 		}
-	}
-	if($db_currentSongId != $db_lastSongId OR mysqli_num_rows($result_getSongId) == 0){
-		$time = time();
-		include "plays.php";
-		echo "done 1live";
-		echo "<br>";
-	}
-	else{
-		echo "Kein neuer Song bei 1Live";
-		echo "<br>";
+		else{
+			echo "Kein neuer Song bei 1Live";
+			echo "<br>";
+		}
 	}
 ?>
--- stations/check_bremenvier.php
+++ stations/check_bremenvier.php
@@ -10,27 +10,30 @@
 	
 	$artistname = mysqli_real_escape_string($conn,strtolower(strip_tags($artists[1])));
 	$songname = mysqli_real_escape_string($conn,strtolower(strip_tags($songs[1])));
-
-	$query_getSongId = "SELECT id FROM song WHERE name = '$songname' AND artist = '$artistname'";
-	$result_getSongId = mysqli_query($conn, $query_getSongId);
-	if(mysqli_num_rows($result_getSongId) >= 1){ 
-		while ($data = mysqli_fetch_array($result_getSongId)){
-			$db_currentSongId = $data['id'];
+	if(empty($artistname) OR empty($songname)){
+		die();
+	}else{
+		$query_getSongId = "SELECT id FROM song WHERE name = '$songname' AND artist = '$artistname'";
+		$result_getSongId = mysqli_query($conn, $query_getSongId);
+		if(mysqli_num_rows($result_getSongId) >= 1){ 
+			while ($data = mysqli_fetch_array($result_getSongId)){
+				$db_currentSongId = $data['id'];
+			}
+			$query_getLastSong = "SELECT songId FROM plays WHERE `stationId` = '$station' ORDER BY `timestamp` DESC LIMIT 1";
+			$result_getLastSong = mysqli_query($conn, $query_getLastSong);
+			while ($data = mysqli_fetch_array($result_getLastSong)){
+				$db_lastSongId = $data['songId'];
+			}
 		}
-		$query_getLastSong = "SELECT songId FROM plays WHERE `stationId` = '$station' ORDER BY `timestamp` DESC LIMIT 1";
-		$result_getLastSong = mysqli_query($conn, $query_getLastSong);
-		while ($data = mysqli_fetch_array($result_getLastSong)){
-			$db_lastSongId = $data['songId'];
+		if($db_currentSongId != $db_lastSongId OR mysqli_num_rows($result_getSongId) == 0){
+			$time = time();
+			include "plays.php";
+			echo "done bremenvier";
+			echo "<br>";
 		}
-	}
-	if($db_currentSongId != $db_lastSongId OR mysqli_num_rows($result_getSongId) == 0){
-		$time = time();
-		include "plays.php";
-		echo "done bremenvier";
-		echo "<br>";
-	}
-	else{
-		echo "Kein neuer Song bei bremenvier";
-		echo "<br>";
+		else{
+			echo "Kein neuer Song bei bremenvier";
+			echo "<br>";
+		}
 	}
 ?>
