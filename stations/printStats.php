9cab8e337c89837531f60050e12c5ebd9224b01e
Timon <timon.borchers@googlemail.com>
2017-10-04 01:02:12 +0200

    *

--- stations/printStats.php
+++ stations/printStats.php
@@ -4,7 +4,7 @@ for($i = 1; $i <= 9; $i++){
 	$station = $i;
 	$time = date("Y-m-d h:i:s",strtotime("-10 minutes",strtotime(date("Y-m-d H:i:s"))));
 	echo $time;
-	$result_getLastPlayTime = mysqli_query($conn, "SELECT * FROM plays WHERE stationId = '$station' AND timestamp LIKE '$time' LIMIT 1")
+	$result_getLastPlayTime = mysqli_query($conn, "SELECT * FROM plays WHERE stationId = '$station' AND timestamp LIKE '$time' LIMIT 1");
 	
 }
 
