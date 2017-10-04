62d0690a1dfb467248dce9881fe849a401f68a8d
Timon <timon.borchers@googlemail.com>
2017-10-04 18:24:27 +0200

    *

--- stations/updateWeekly.php
+++ stations/updateWeekly.php
@@ -9,7 +9,7 @@
 		$station = $i;
 		$query_replaysPerDay = "SELECT replaysPerDay FROM dailyStats WHERE stationId = '$station' AND YEARWEEK(`timestamp`, 1) = YEARWEEK(CURDATE(), 1)";
 		$result_replaysPerDay = mysqli_query($conn, $query_replaysPerDay);
-		while($data = mysqli_fetch_array($result_replaysPerDay){
+		while($data = mysqli_fetch_array($result_replaysPerDay)){
 			$db_replaysPerDay = $data['replaysPerDay'];
 			$replaysPerWeek = $replaysPerDay + $db_replaysPerDay;
 		}
