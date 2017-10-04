3bdbe68e61ece17e94b87a95de1241b899811ec3
Timon <timon.borchers@googlemail.com>
2017-10-04 22:45:09 +0200

    *

--- stations/printStats.php
+++ stations/printStats.php
@@ -1,14 +1,23 @@
 <?php
 	include_once "db.php";
+	
+	$currentMonth = date("Y-m");			
+	$currentDay = date("Y-m-d");			
+	$currentHour = date("Y-m-d H");			
+	$currentSeconds = date("Y-m-d H:i:s");	
+	
 	$max = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM station"));					//check how many stations we have
 	for($i = 1; $i <= $max; $i++){							
 		$station = $i;
 		
-		$stationName = mysqli_query($conn,"SELECT name FROM station WHERE id = '$station'");
-		while($data = mysqli_fetch_array($stationName)){
+		$getstationName = mysqli_query($conn,"SELECT name FROM station WHERE id = '$station'");
+		while($data = mysqli_fetch_array($getstationName)){
 			$db_stationName = $data['name'];
 		}
-		
+		$getReplaysPerHour = mysqli_query($conn, "SELECT replaysPerHour FROM hourlyStats WHERE stationId = '$station' AND timestamp LIKE '$currentHour'")
+		while($data = mysqli_fetch_array($getReplaysPerHour)){
+			$db_replaysPerHour = $data['replaysPerHour'];
+		}
 		
 		
 		
@@ -20,7 +29,7 @@
 		
 		echo"<tr>";
                   echo"<td>" . $db_stationName . "</td>";
-                  echo"<td>0</td>";
+                  echo"<td>" . $db_replaysPerHour . "</td>";
                   echo"<td>54</td>";
                   echo"<td>100</td>";
                   echo"<td>999</td>";
