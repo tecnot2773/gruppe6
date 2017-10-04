6938974c99e29801b756c28d4adace64fd7c8875
Timon <timon.borchers@googlemail.com>
2017-10-04 20:02:06 +0200

    *

--- stations/updateMonthly.php
+++ stations/updateMonthly.php
@@ -5,6 +5,7 @@
 	$max = mysqli_num_rows(mysqli_query($conn, "SELECT * FROM station"));					//check how many stations we have
 	for($i = 1; $i <= $max; $i++){
 		$station = $i;
+		$replaysPerWeek = 0;
 		$query_replaysPerDay = "SELECT replaysPerDay FROM dailyStats WHERE stationId = '$station' AND timestamp LIKE '$currentMonth%'";
 		$result_replaysPerDay = mysqli_query($conn, $query_replaysPerDay);
 		while($data = mysqli_fetch_array($result_replaysPerDay)){
@@ -18,14 +19,14 @@
 		$avgReplaysPerDay = round($avgReplaysPerDay, 2);
 		$avgReplaysPerDay = number_format($avgReplaysPerDay, 2);
 		echo $avgReplaysPerDay. "<br>";
-		mysqli_query($conn, "UPDATE monthlyStats SET replaysPerDay = '$avgReplaysPerDay' WHERE stationId = '$station' AND timestamp LIKE '$currentMonth%");
+		mysqli_query($conn, "UPDATE monthlyStats SET replaysPerDay = '$avgReplaysPerDay' WHERE stationId = '$station' AND timestamp LIKE '$currentMonth%'");
 		
-		$result_getMostPlayedSong = (mysqli_query($conn, "SELECT `songId`, COUNT(`songId`) AS `value_occurrence` FROM `plays` WHERE `stationId`= '$station' AND timestamp LIKE '$currentMonth% GROUP BY `songId` ORDER BY `value_occurrence` DESC LIMIT 1"));
+		$result_getMostPlayedSong = (mysqli_query($conn, "SELECT `songId`, COUNT(`songId`) AS `value_occurrence` FROM `plays` WHERE `stationId`= '$station' AND timestamp LIKE '$currentMonth%' GROUP BY `songId` ORDER BY `value_occurrence` DESC LIMIT 1"));
 		while($data = mysqli_fetch_array($result_getMostPlayedSong)){
 			$db_mostPlayed = $data['songId'];
 			$db_mostPlayedCount = $data['value_occurrence'];
 		}
-		mysqli_query($conn, "UPDATE monthlyStats SET mostPlayedSong = '$db_mostPlayed', count = '$db_mostPlayedCount' WHERE stationId = '$station' AND timestamp LIKE '$currentMonth%");	
+		mysqli_query($conn, "UPDATE monthlyStats SET mostPlayedSong = '$db_mostPlayed', count = '$db_mostPlayedCount' WHERE stationId = '$station' AND timestamp LIKE '$currentMonth%'");	
 	}
 	
 ?>
