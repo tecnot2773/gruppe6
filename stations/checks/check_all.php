b6c86cc35edb00a569a38a2f5e850e798567bb88
Timon <timon.borchers@googlemail.com>
2017-10-04 18:23:24 +0200

    *

--- stations/checks/check_all.php
+++ stations/checks/check_all.php
@@ -41,6 +41,7 @@
 		$station = $i;
 		if(mysqli_num_rows(mysqli_query($conn, "SELECT * FROM weeklyStats WHERE stationId = '$station' and timestamp BETWEEN '$monday' AND '$sunday'")) == 0){
 			mysqli_query($conn, "INSERT INTO weeklyStats (stationId, timestamp, replaysPerWeek, replaysPerDay, score) VALUES ('$station', '$currentSeconds',  '0', '0', '0')");
+			echo "123";
 		}
 		if(mysqli_num_rows(mysqli_query($conn, "SELECT * FROM dailyStats WHERE stationId = '$station' and timestamp LIKE '$currentDay%'")) == 0){					//Check if dailystats exitst for current day																														//if not
 			mysqli_query($conn, "INSERT INTO dailyStats (stationId, timestamp, replaysPerHour, replaysPerDay, mostReplaysDuring, score) VALUES ('$station', '$currentSeconds', '0', '0', '0', '0')");	//Insert new daiylstats with current timestamp
