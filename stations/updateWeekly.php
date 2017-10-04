d74c89a3aaaa680627438c0f7c1f97cdb2be2ed7
Timon <timon.borchers@googlemail.com>
2017-10-04 19:06:08 +0200

    *

--- stations/updateWeekly.php
+++ stations/updateWeekly.php
@@ -16,7 +16,7 @@
 		//ReplaysPerDay Average
 		$days = mysqli_num_rows($result_replaysPerDay);
 		$avgReplaysPerDay = $replaysPerWeek / $days;
-		$avgReplaysPerDayround = round($avgReplaysPerDay, 2);
+		$avgReplaysPerDay= round($avgReplaysPerDay, 2);
 		echo $avgReplaysPerDay. "<br>";
 		mysqli_query($conn, "UPDATE weeklyStats SET replaysPerDay = '$avgReplaysPerDay' WHERE stationId = '$station' AND YEARWEEK(`timestamp`, 1) = YEARWEEK(CURDATE(), 1)");
 		
