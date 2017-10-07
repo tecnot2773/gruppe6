80ebe91b66676a35ce3255c93c6a875fce80c17e
Timon <timon.borchers@googlemail.com>
2017-10-07 16:13:25 +0200

    move

--- stations/updateHourly.php
+++ stations/updateHourly.php
@@ -22,18 +22,6 @@
 			$replaysPerHour = $replaysPerHour + ($replays - 1);
 		}
 		mysqli_query($conn, "UPDATE hourlyStats SET replaysPerHour = '$replaysPerHour' WHERE stationId = '$station' AND timestamp LIKE '$currentHour%'");									//replaysPerHour+1
-
-		$calc_replays = 0;
-		$replaysPerDay = 0;
-		$query_getHourlyStats = "SELECT replaysPerHour FROM hourlyStats WHERE stationId = '$station' AND timestamp LIKE '$currentDay%'";					//get replaysperhour from hourlystats
-		$result_getHourlyStats = mysqli_query($conn, $query_getHourlyStats);
-		$rows_getHourlyStats = mysqli_num_rows($result_getHourlyStats);
-		while($data = mysqli_fetch_array($result_getHourlyStats)){
-			$db_replaysPerHour = $data['replaysPerHour'];																								//fetch replays from every hour
-			$calc_replays = $calc_replays + $db_replaysPerHour;																							//add
-		}
-		$calc_replays = ($calc_replays / $rows_getHourlyStats);																							//calculate average
-		$calc_replays = round($calc_replays, 2);
 		
 	}
 ?>
