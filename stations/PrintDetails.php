c26b2d5afe9637fb7ae0d19bc92256188ee76982
Timon <timon.borchers@googlemail.com>
2017-10-15 17:57:04 +0200

    *

--- stations/PrintDetails.php
+++ stations/PrintDetails.php
@@ -53,8 +53,13 @@
 		for($i = 0; $i <= 6; $i++){
 			$day = date('Y-m-d', strtotime("monday last week +$i Days"));		//get monday last week + i
 			$getReplaysPerDayChart = mysqli_query($conn, "SELECT replaysPerDay FROM dailyStats WHERE stationId = '$station' AND timestamp LIKE '$day%'");
-			while($data = mysqli_fetch_array($getReplaysPerDayChart)){
-				$db_DayChart = $data['replaysPerDay'];
+			if(mysqli_num_rows($getReplaysPerDayChart) >= 1){
+				while($data = mysqli_fetch_array($getReplaysPerDayChart)){
+					$db_DayChart = $data['replaysPerDay'];
+					$weekChart = $weekChart . $db_DayChart . ", ";		//build string
+				}
+			}else{
+				$db_DayChart = 0;
 				$weekChart = $weekChart . $db_DayChart . ", ";		//build string
 			}
 		}
