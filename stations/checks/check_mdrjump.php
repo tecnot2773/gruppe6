14a4cf63d6b2a25a9a51a95f4b69cfa7c69875d6
FranklinClinton <joern@team-runge.de>
2017-10-16 21:28:34 +0200

    *

--- stations/checks/check_mdrjump.php
+++ stations/checks/check_mdrjump.php
@@ -7,11 +7,9 @@
 	$http_content = file_get_contents("http://www.jumpradio.de/XML/titellisten/jump_onair.json");
 	$json = json_decode($http_content, true); // decode the JSON into an associative array
 	
-	echo ($json['Songs'][0]['title']);
 	$artistname = mysqli_real_escape_string($conn,strtolower($json['Songs'][0]['title']));
 	$songname = mysqli_real_escape_string($conn,strtolower($json['Songs'][0]['interpret']));
 	
-	die();
 	if(empty($artistname) OR empty($songname)){
 		
 	}else{
