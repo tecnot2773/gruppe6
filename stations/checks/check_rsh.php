8f191f65d1a09081854314db0495bfcd67e9372f
FranklinClinton <joern@team-runge.de>
2017-10-16 20:57:40 +0200

    *

---
+++ stations/checks/check_rsh.php
@@ -0,0 +1,39 @@
+<?php
+
+	$station = 1;
+	$db_currentSongId = 0;
+	$db_lastSongId = 0;
+	
+	$http_content = file_get_contents("http://iris-rsh.loverad.io/flow.json?station=2&count=2&_=1508178220099");
+	$json = json_decode($http_content, true); // decode the JSON into an associative array
+	echo '<pre>' . print_r($json, true) . '</pre>';
+	die();
+	$artistname = mysqli_real_escape_string($conn,strtolower(strip_tags($artists[1])));
+	$songname = mysqli_real_escape_string($conn,strtolower(strip_tags($songs[1])));
+	if(empty($artistname) OR empty($songname)){
+		
+	}else{
+		$query_getSongId = "SELECT id FROM song WHERE name = '$songname' AND artist = '$artistname'";
+		$result_getSongId = mysqli_query($conn, $query_getSongId);
+		if(mysqli_num_rows($result_getSongId) >= 1){ 
+			while ($data = mysqli_fetch_array($result_getSongId)){
+				$db_currentSongId = $data['id'];
+			}
+			$query_getLastSong = "SELECT songId FROM plays WHERE `stationId` = '$station' ORDER BY `timestamp` DESC LIMIT 1";
+			$result_getLastSong = mysqli_query($conn, $query_getLastSong);
+			while ($data = mysqli_fetch_array($result_getLastSong)){
+				$db_lastSongId = $data['songId'];
+			}
+		}
+		if($db_currentSongId != $db_lastSongId OR mysqli_num_rows($result_getSongId) == 0){
+			$time = time();
+			include "../plays.php";
+			echo "done ffn";
+			echo "<br>";
+		}
+		else{
+			echo "Kein neuer Song bei ffn";
+			echo "<br>";
+		}
+	}
+?>
