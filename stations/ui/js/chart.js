c4832b2c0381d52707a03e2ad7f8579a689d5e4c
FranklinClinton <joern@team-runge.de>
2017-10-16 18:00:06 +0200

    *

--- stations/ui/js/chart.js
+++ stations/ui/js/chart.js
@@ -30,6 +30,8 @@ var option = {
   }
 };
 var HTMLdata = httpGet("https://gruppe6.torutec.eu/stations/PrintDetails.php?type=weekChart&station=" + station_id);
+print(HTMLdata);
+print("[" + HTMLdata + "]");
 var dataArray = JSON.parse("[" + HTMLdata + "]");
 alert(dataArray);
 var weeklyChart = new Chart(weeklyChart, {
