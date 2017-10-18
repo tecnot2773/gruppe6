c243be42870974bef516a797961a192da75da70a
Timon <timon.borchers@googlemail.com>
2017-10-18 10:50:44 +0200

    *

---
+++ include/include_tollgate.php
@@ -0,0 +1,16 @@
+<?php
+
+	class Tollgate
+	{
+		public static function getHighway()
+		{
+			include_once '../include/include_db.php';																	//create db connection
+			
+			$query_getHighway = "SELECT nameAutobahn from mautstelle";											//sql query to get  kennzeichen
+			$result_getHighway = mysqli_query($conn,$query_getHighway);												//execute query and save
+			while($data = mysqli_fetch_array($result_getHighway)){												//fetch data from result_getPlate
+				echo '<option value="' . $data['nameAutobahn'] . '">' . $data['nameAutobahn']. '</option>';		//use echo to execute html in php
+			}
+		}
+
+?>
