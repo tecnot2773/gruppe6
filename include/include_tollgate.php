d22d5c47ab75a5847bdec57885d7a6e2f94a1546
Timon <timon.borchers@googlemail.com>
2017-10-18 11:59:26 +0200

    *

--- include/include_tollgate.php
+++ include/include_tollgate.php
@@ -5,7 +5,7 @@
 		public static function getHighway($conn)
 		{
 
-			$query_getHighway = "SELECT DISTINCT nameAutobahn FROM `mautstelle`";											//sql query to get  kennzeichen
+			$query_getHighway = "SELECT DISTINCT nameAutobahn FROM mautstelle ORDER BY SUBSTR(nameAutobahn FROM 1 FOR 1), CAST(SUBSTR(nameAutobahn FROM 2) AS UNSIGNED)";											//sql query to get  kennzeichen
 			$result_getHighway = mysqli_query($conn,$query_getHighway);												//execute query and save
 			while($data = mysqli_fetch_array($result_getHighway)){												//fetch data from result_getPlate
 				echo '<option value="' . $data['nameAutobahn'] . '">' . $data['nameAutobahn']. '</option>';		//use echo to execute html in php
