06a9b7583a579cf052a15cef55696f722215bc09
Timon <timon.borchers@googlemail.com>
2017-10-22 17:05:12 +0200

    *

--- test/test.php
+++ test/test.php
@@ -5,7 +5,7 @@
 
 	$http_content = file_get_contents("http://ptr.wowhead.com/npc=" .$npc);
 	
-	preg_match('/"name":"4(.+?)(?=")/', $http_content, $itemName);
+	preg_match_all('/"name":"4(.+?)(?=")/', $http_content, $itemName);
 	$len=count($itemName);
 	for ($i=0; $i<$len ;$i++){
 		echo $itemName[$i][1];
