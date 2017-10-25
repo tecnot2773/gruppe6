15b462490b24dd82cb682027438af13780729945
tecnot <tecnot2773@users.noreply.github.com>
2017-10-25 10:11:19 +0200

    Update chart.js

--- stations/ui/js/chart.js
+++ stations/ui/js/chart.js
@@ -53,7 +53,7 @@ var monthlyChart = new Chart(monthlyChart, {
     labels: ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'],
     datasets: [{
       label: 'Wiederholungen',
-      data: JSON.parse("[" + httpGet("https://gruppe6.torutec.eu/stations/PrintDetails.php?type=yearChartChart&station=" + station_id) + "]"),
+      data: JSON.parse("[" + httpGet("https://gruppe6.torutec.eu/stations/PrintDetails.php?type=yearChart&station=" + station_id) + "]"),
       backgroundColor: "rgba(153,51,255,0.4)"
     }]
   }
