75ec25343aa6a8a897b768451e5834b56db00a14
Timon <timon.borchers@googlemail.com>
2017-10-25 12:51:00 +0200

    *

--- include/include_statistics.php
+++ include/include_statistics.php
@@ -67,7 +67,7 @@ class Statistic
 				$result_monthyCount = mysqli_query($conn, "SELECT * FROM faehrtAus WHERE zeitstempel LIKE '$month$'");
 				$monthlyCount = mysqli_num_rows($result_monthyCount);
 				
-				echo "<td width='350px'> Autos diesen Monat auf der Autobahn </td>";
+				echo "<td width='350px'> Autos im Monat ${i} auf der Autobahn </td>";
 				echo "<td width='350px'> ${monthlyCount} </td>";
 				echo "</tr>";
 			}
