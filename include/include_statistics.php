d362e9c4d3b6fc1fdc0a2dc92f50020d85e83b40
Timon <timon.borchers@googlemail.com>
2017-10-26 12:43:37 +0200

    make monthname german

--- include/include_statistics.php
+++ include/include_statistics.php
@@ -6,11 +6,11 @@ class Statistic
 			$query_tollgateNumber = "SELECT * FROM mautstelle";
 			$result_tollgateNumber = mysqli_query($conn,$query_tollgateNumber);
 			$tollgateNumberRows = mysqli_num_rows($result_tollgateNumber);
-			
+
 			echo $tollgateNumberRows;
 		}
 		public static function entryCount($conn)
-		{	
+		{
 			$query_entryNumber = "SELECT * FROM faehrtEin";
 			$result_entryNumber = mysqli_query($conn,$query_entryNumber);
 			$entryNumberRows = mysqli_num_rows($result_entryNumber);
@@ -22,54 +22,55 @@ class Statistic
 			$query_exitNumber = "SELECT * FROM faehrtAus";
 			$result_exitNumber = mysqli_query($conn,$query_exitNumber);
 			$exitNumberRows = mysqli_num_rows($result_exitNumber);
-			
+
 			echo $exitNumberRows;
 		}
 		public static function onTheRoad($conn)
-		{		
+		{
 			$query_entryNumber = "SELECT * FROM faehrtEin";
 			$result_entryNumber = mysqli_query($conn,$query_entryNumber);
-			$entryNumberRows = mysqli_num_rows($result_entryNumber);		
-		
-		
+			$entryNumberRows = mysqli_num_rows($result_entryNumber);
+
+
 			$query_exitNumber = "SELECT * FROM faehrtAus";
 			$result_exitNumber = mysqli_query($conn,$query_exitNumber);
 			$exitNumberRows = mysqli_num_rows($result_exitNumber);
 			$onTheRoad = $entryNumberRows - $exitNumberRows;
-			
+
 			echo $onTheRoad;
 		}
 		public static function dailyExit($conn)
-		{	
+		{
 			$currentDay = date("Y-m-d");																				//get current date in form DD.MM.YYYY
 
 			$query_getDailyExit = "SELECT * FROM faehrtAus WHERE zeitstempel LIKE '$currentDay%'";						//SELECT where zeitstempel is current day
 			$result_getDailyExit = mysqli_query($conn,$query_getDailyExit);
 			$dailyExitRows = mysqli_num_rows($result_getDailyExit);														//get rows from previous select
-			
+
 			echo $dailyExitRows;
 		}
 		public static function monthlyExit($conn)
-		{	
+		{
 			$currentMonth = date("Y-m");																				//get current date in form MM.YYYY
 			$query_getMonthlyExit = "SELECT * FROM faehrtAus WHERE zeitstempel LIKE '%$currentMonth%'";					//SELECT where zeitstempel is curren month
 			$result_getMonthlyExit = mysqli_query($conn,$query_getMonthlyExit);
 			$monthlyExitRows = mysqli_num_rows($result_getMonthlyExit);													//get rows from previous select
-			
+
 			echo $monthlyExitRows;
 		}
 		public static function monthlyCount($conn)
 		{
+			setlocale (LC_ALL, 'de_DE@euro', 'de_DE', 'de', 'ge');
 			for($i = 1; $i < 13; $i++){
 				$month = date("Y");
 				$month = $month . "-" . $i;
 				$result_monthyCount = mysqli_query($conn, "SELECT * FROM faehrtAus WHERE zeitstempel LIKE '$month%'");
 				$monthlyCount = mysqli_num_rows($result_monthyCount);
-				
+
 				$dateObj   = DateTime::createFromFormat('!m', $i);
 				$monthName = $dateObj->format('F');
-				
-				
+
+
 				echo "<td width='350px'> Autos im ${monthName} auf der Autobahn </td>";
 				echo "<td width='350px'> ${monthlyCount} </td>";
 				echo "</tr>";
@@ -79,13 +80,13 @@ class Statistic
 		{
 			$start = $_POST["startSearch"];
 			$end = $_POST["endSearch"];
-			
+
 			$start = date("Y-m-d H:i:s", strtotime($start));
 			$end = date("Y-m-d H:i:s", strtotime($end));
-		
+
 			$result_searchCount = mysqli_query($conn, "SELECT * FROM faehrtAus WHERE zeitstempel BETWEEN '$start' and '$end'");
 			$searchCount = mysqli_num_rows($result_searchCount);
-			
+
 			echo $searchCount;
 		}
 	}
