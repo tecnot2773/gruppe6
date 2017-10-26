5c51541a476d226415bb21dbccabdfa586c5604c
Unknown <joern@team-runge.de>
2017-10-26 13:03:22 +0200

    lala*

--- mitarbeiter/index.php
+++ mitarbeiter/index.php
@@ -44,6 +44,10 @@
 				</div>
 				<div class="jumbo-white">
 						<div class="row-radio">
+							<input id="text-IDentry" name="text-CodeEntry" class="enjoy-css" type="text" placeholder="Code Einfahrt">
+							<input id="text-time-entry" name="text-time-entry" class="enjoy-css" type="text" placeholder="DD.MM.YYYY HH:MM:SS">
+						</div>
+						<div class="row-radio">
 							<label>
 								<input type="radio" name="selection" value="entry" checked="checked">
 								Einfahrt
@@ -51,8 +55,6 @@
 						</div>
 						<div id="first-row" class="row-data">
 							<input id="text-plate-entry" name="text-plate-entry" class="enjoy-css" type="text" placeholder="Kennzeichen">
-							<input id="text-IDentry" name="text-CodeEntry" class="enjoy-css" type="text" placeholder="Code Einfahrt">
-							<input id="text-time-entry" name="text-time-entry" class="enjoy-css" type="text" placeholder="DD.MM.YYYY HH:MM:SS">
 						</div>
 
 
@@ -75,8 +77,6 @@
 									}
 								?>
 							</select>
-							<input id="text-IDexit" name="text-CodeExit" class="enjoy-css" type="text" placeholder="Code Ausfahrt">
-							<input id="text-time-exit" name="text-time-exit" class="enjoy-css" type="text" placeholder="DD.MM.YYYY HH:MM:SS">
 							<input class="buttonsmall" type="submit" id="execute" name="execute" value="Ausführen">
 						</div>
 						<div class="row-bottom">
