4de7da1eb65430c8c3203d3f5d46345c30b83a41
Timon <timon.borchers@googlemail.com>
2017-10-27 16:03:39 +0200

    ;

--- include/include_bill.php
+++ include/include_bill.php
@@ -61,7 +61,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	include_once 'include_db.php';																										//include db.php
 	$plate = $_POST["text-search-kennzeichen"];
 
-	$plateCheck = mysqli_query($conn, "SELECT id FROM strecke WHERE kennzeichen = '$plate';")
+	$plateCheck = mysqli_query($conn, "SELECT id FROM strecke WHERE kennzeichen = '$plate'");
 	if(mysqli_num_rows($plateCheck) > 0){
 		$query_getBill = "SELECT * FROM rechnung r JOIN strecke s ON r.streckeID = s.id WHERE s.kennzeichen = '$plate' ORDER BY r.id DESC";												//query getBill
 		$result_getBill = mysqli_query($conn,$query_getBill);																		//execute query and save
