78a5b52cbae6a0326caa18492d16ac6b4cd15434
Timon <timon.borchers@googlemail.com>
2017-10-27 14:11:22 +0200

    format

--- include/include_functionEntryExit.php
+++ include/include_functionEntryExit.php
@@ -21,29 +21,29 @@ class EntryExit{
 	}
 	public static function entry($conn)
 	{
-		echo "<div class='row-radio'>\r\n";
-			echo "<label>\r\n";
-				echo "<input type='radio' name='selection' value='entry' checked='checked'>\r\n";
-				echo "Einfahrt\r\n";
-			echo "</label>\r\n";
-			echo "<label>\r\n";
-				echo "<input type='radio' name='selection' value='exit'>\r\n";
-				echo "Ausfahrt\r\n";
-			echo "</label>\r\n";
-		echo "</div>\r\n";
-		echo "<select name='text-Autobahn' class='enjoy-css'>\r\n";
-			echo "<option value='' disabled='' selected='' hidden=''>Autobahn</option>\r\n";
-			$result_Highway = mysqli_query($conn,"SELECT DISTINCT nameAutobahn from mautstelle ORDER BY SUBSTR(nameAutobahn FROM 1 FOR 1), CAST(SUBSTR(nameAutobahn FROM 2) AS UNSIGNED)");												//execute query and save
-			while($data = mysqli_fetch_array($result_Highway)){												//fetch data from result_getPlate
-			echo '<option value="' . $data['nameAutobahn'] . '">' . $data['nameAutobahn']. '</option>' . "\r\n";		//use echo to execute html in php
-			}
-		echo "</select>\r\n";
+		echo "\t\t\t\t\t<div class='row-radio'>\r\n";
+		echo "\t\t\t\t\t\t<label>\r\n";
+		echo "\t\t\t\t\t\t\t<input type='radio' name='selection' value='entry' checked='checked'>\r\n";
+		echo "\t\t\t\t\t\t\tEinfahrt\r\n";
+		echo "\t\t\t\t\t\t</label>\r\n";
+		echo "\t\t\t\t\t\t<label>\r\n";
+		echo "\t\t\t\t\t\t\t<input type='radio' name='selection' value='exit'>\r\n";
+		echo "\t\t\t\t\t\t\tAusfahrt\r\n";
+		echo "\t\t\t\t\t\t</label>\r\n";
+		echo "\t\t\t\t\t</div>\r\n";
+		echo "\t\t\t\t\t\t<select name='text-Autobahn' class='enjoy-css'>\r\n";
+		echo "\t\t\t\t\t\t\t<option value='' disabled='' selected='' hidden=''>Autobahn</option>\r\n";
+		$result_Highway = mysqli_query($conn,"SELECT DISTINCT nameAutobahn from mautstelle ORDER BY SUBSTR(nameAutobahn FROM 1 FOR 1), CAST(SUBSTR(nameAutobahn FROM 2) AS UNSIGNED)");												//execute query and save
+		while($data = mysqli_fetch_array($result_Highway)){												//fetch data from result_getPlate
+			echo "\t\t\t\t\t\t\t" . '<option value="' . $data['nameAutobahn'] . '">' . $data['nameAutobahn']. '</option>' . "\r\n";		//use echo to execute html in php
+		}
+		echo "\t\t\t\t\t\t</select>\r\n";
 
-		echo "<div class='row-data'>\r\n";
-		echo "<input class='buttonsmall' type='submit' name='execute' value='Weiter'>\r\n";
-		echo "</div>\r\n";
-		echo "<div class='row-bottom'>\r\n";
-		echo "</div>\r\n";
+		echo "\t\t\t\t\t\t<div class='row-data'>\r\n";
+		echo "\t\t\t\t\t\t\t<input class='buttonsmall' type='submit' name='execute' value='Weiter'>\r\n";
+		echo "\t\t\t\t\t\t</div>\r\n";
+		echo "\t\t\t\t\t\t<div class='row-bottom'>\r\n";
+		echo "\t\t\t\t\t\t</div>\r\n";
 	}
 	public static function exit($conn)
 	{
