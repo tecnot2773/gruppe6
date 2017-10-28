48be4572e723ea9621564ae71c0920b74af20b66
Timon <timon.borchers@googlemail.com>
2017-10-28 18:54:16 +0200

    *

--- mitarbeiter/detail/index.php
+++ mitarbeiter/detail/index.php
@@ -57,11 +57,11 @@
           </tr>
           <tr>
             <td colspan="4"></td>
-            <td class="total"><?php if($cost != $calcCosts){echo $costs;} ?></td>
+            <td class="total"><?php if($costs != $calcCosts){echo $costs;} ?></td>
           </tr>
           <tr>
             <td colspan="4"> </td>
-            <td class="total"><?php if($cost != $calcCosts){echo "-5%";} ?></td>
+            <td class="total"><?php if($costs != $calcCosts){echo "-5%";} ?></td>
           </tr>
           <tr>
             <td colspan="4" class="grand total">Summe</td>
